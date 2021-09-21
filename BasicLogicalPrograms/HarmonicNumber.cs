7bd7ffa60bf3407feaaf82e6c154eb8933cef275
nibedita260 <deepi.nibedita260.com>
2021-09-21 19:21:25 +0530

    [Nibedita]Added:HarmonicNumber Program Feature Implemented

---
+++ BasicLogicalPrograms/HarmonicNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicLogicalPrograms
+{
+    class HarmonicNumber
+    {
+        public void PrintHarmonicNum()
+        {
+            int N;
+            float harmonic = 1;
+            Console.WriteLine("Enter number to print Nth Harmonic number");
+            N = Convert.ToInt32(Console.ReadLine());
+            for(int i=2;i<=N;i++)
+            {
+                harmonic += (float)1 / i;
+            }
+            Console.WriteLine("The Nth Harmonic number:" + harmonic);
+        }
+    }
+}
--- BasicLogicalPrograms/Program.cs
+++ BasicLogicalPrograms/Program.cs
@@ -7,7 +7,7 @@ namespace BasicLogicalPrograms
         static void Main(string[] args)
         {
             int N;
-            Console.WriteLine("Choose 1.FlipCoin 2.LeapYear 3.PowerOfTwo");
+            Console.WriteLine("Choose 1.FlipCoin 2.LeapYear 3.PowerOfTwo 4.HarmonicNumber");
             N = Convert.ToInt32(Console.ReadLine());
             switch (N)
             {
@@ -22,6 +22,10 @@ namespace BasicLogicalPrograms
                 case 3:
                     PowerOfTwo powerOfTwo = new PowerOfTwo();
                     powerOfTwo.PowerOfTwoTable();
+                    break;
+                case 4:
+                    HarmonicNumber harmonicNumber = new HarmonicNumber();
+                    harmonicNumber.PrintHarmonicNum();
                     break;
                 default:
                     Console.WriteLine("Choose valid option");
