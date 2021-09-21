73de1e08bc7ca2ade68753a99bd4f2b38f313c75
nibedita260 <deepi.nibedita260.com>
2021-09-21 19:15:07 +0530

    [Nibedita]Added:LeapYear Program Feature Implemented

--- BasicLogicalPrograms/Program.cs
+++ BasicLogicalPrograms/Program.cs
@@ -7,13 +7,17 @@ namespace BasicLogicalPrograms
         static void Main(string[] args)
         {
             int N;
-            Console.WriteLine("Choose 1.FlipCoin");
+            Console.WriteLine("Choose 1.FlipCoin 2.LeapYear");
             N = Convert.ToInt32(Console.ReadLine());
             switch (N)
             {
                 case 1:
                     FlipCoin flipCoin = new FlipCoin();
                     flipCoin.CoinToss();
+                    break;
+                case 2:
+                    CheckLeapYear checkLeapYear = new CheckLeapYear();
+                    checkLeapYear.CheckLeapYr();
                     break;
                 default:
                     Console.WriteLine("Choose valid option");
