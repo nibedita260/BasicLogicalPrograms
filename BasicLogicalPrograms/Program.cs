a0b2309ba5b11ac79d5a9878817ca40d8662e8f1
nibedita260 <deepi.nibedita260.com>
2021-09-21 17:29:06 +0530

    BasicCorePrograms

--- BasicLogicalPrograms/Program.cs
+++ BasicLogicalPrograms/Program.cs
@@ -7,13 +7,25 @@ namespace BasicLogicalPrograms
         static void Main(string[] args)
         {
             int N;
-            Console.WriteLine("Choose 1.FlipCoin 6.QuotientandRemainder 7.SwapOfTwoNumbers 8:CheckEvenOrOdd 9.CheckVowelOrConsonat 10.LargestAmongThreeNumbers");
+            Console.WriteLine("Choose 1.FlipCoin 2.CheckleapYear 3.PowerOfTwo 4.HarmonicNumber 5.PrimeFactors 6.QuotientandRemainder 7.SwapOfTwoNumbers 8:CheckEvenOrOdd 9.CheckVowelOrConsonat 10.LargestAmongThreeNumbers");
             N = Convert.ToInt32(Console.ReadLine());
             switch (N)
             {
                 case 1:
                     FlipCoin flipCoin = new FlipCoin();
                     flipCoin.CoinToss();
+                    break;
+                case 2:CheckLeapYear checkLeapYear = new CheckLeapYear();
+                    checkLeapYear.CheckLeapYr();
+                    break;
+                case 3:PowerOfTwo powerOfTwo = new PowerOfTwo();
+                    powerOfTwo.PowerOfTwoTable();
+                    break;
+                case 4: HarmonicNumber harmonicNumber= new HarmonicNumber();
+                    harmonicNumber.PrintHarmonicNum();
+                    break;
+                case 5:PrimeFactor primeFactor = new PrimeFactor();
+                    primeFactor.PrintPrimeFactors();
                     break;
                 case 6:
                     QuotientandRemainder quotientandRemainder = new QuotientandRemainder();
