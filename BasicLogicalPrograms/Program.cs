10436c13a81705a8b51bceb986b13ce9fc8dc326
nibedita260 <deepi.nibedita260.com>
2021-09-21 19:10:14 +0530

    [Nibedita]Added:FlipCoin Prog Feature Implemented

--- BasicLogicalPrograms/Program.cs
+++ BasicLogicalPrograms/Program.cs
@@ -7,29 +7,13 @@ namespace BasicLogicalPrograms
         static void Main(string[] args)
         {
             int N;
-            Console.WriteLine("Choose 1.FlipCoin 2.LeapYear 3.PowerOfTwo 4.HarmonicNumber 5.PrimeFactors");
+            Console.WriteLine("Choose 1.FlipCoin");
             N = Convert.ToInt32(Console.ReadLine());
             switch (N)
             {
                 case 1:
                     FlipCoin flipCoin = new FlipCoin();
                     flipCoin.CoinToss();
-                    break;
-                case 2:
-                    CheckLeapYear checkLeapYear = new CheckLeapYear();
-                    checkLeapYear.CheckLeapYr();
-                    break;
-                case 3:
-                    PowerOfTwo powerOfTwo = new PowerOfTwo();
-                    powerOfTwo.PowerOfTwoTable();
-                    break;
-                case 4:
-                    HarmonicNumber harmonicNumber = new HarmonicNumber();
-                    harmonicNumber.PrintHarmonicNum();
-                    break;
-                case 5:
-                    PrimeFactor primeFactor = new PrimeFactor();
-                    primeFactor.PrintPrimeFactors();
                     break;
                 default:
                     Console.WriteLine("Choose valid option");
