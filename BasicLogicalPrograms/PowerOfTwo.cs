a2d36fe9856ab72e5663d6e831f2050eccc6732d
nibedita260 <deepi.nibedita260.com>
2021-09-22 13:03:04 +0530

    [Nibedita]Added:QuotientandRemainder Program Feature Implemented

--- BasicLogicalPrograms/PowerOfTwo.cs
+++ BasicLogicalPrograms/PowerOfTwo.cs
@@ -6,14 +6,15 @@ namespace BasicLogicalPrograms
 {
     class PowerOfTwo
     {
-        public void PowerOfTwoTable() {
-            UInt64 N;// since 2^31 to not overflows an int
-            Console.WriteLine("Enter number to print power of two of that number");
-            N = Convert.ToUInt64(Console.ReadLine());
-                for (int i = 1; i <= Math.Pow(2, N); i++)
-                {
-                    Console.WriteLine("The table of the powers of 2:" + Math.Pow(2,i));
-                }
+        public void PowerOfTwoTable()
+        {
+            int n;// since 2^31 overflows an int ,take 0<=n<31
+            Console.WriteLine("Enter number to print till power of two of that number");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("The table of the powers of 2:" + Math.Pow(2, i));
+            }
         }
     }
 }
