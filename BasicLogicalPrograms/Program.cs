93d83eccc752ca98073a6634c8cd627e03da45dc
nibedita260 <deepi.nibedita260.com>
2021-09-22 12:48:58 +0530

    [Nibedita]Added:HarmonicNumber Program Feature Implemented

--- BasicLogicalPrograms/Program.cs
+++ BasicLogicalPrograms/Program.cs
@@ -6,30 +6,35 @@ namespace BasicLogicalPrograms
     {
         static void Main(string[] args)
         {
-            int N;
-            Console.WriteLine("Choose 1.FlipCoin 2.LeapYear 3.PowerOfTwo 4.HarmonicNumber");
-            N = Convert.ToInt32(Console.ReadLine());
-            switch (N)
+            int n;
+            bool IsExitProgram = false;
+            while (!IsExitProgram)
             {
-                case 1:
-                    FlipCoin flipCoin = new FlipCoin();
-                    flipCoin.CoinToss();
-                    break;
-                case 2:
-                    CheckLeapYear checkLeapYear = new CheckLeapYear();
-                    checkLeapYear.CheckLeapYr();
-                    break;
-                case 3:
-                    PowerOfTwo powerOfTwo = new PowerOfTwo();
-                    powerOfTwo.PowerOfTwoTable();
-                    break;
-                case 4:
-                    HarmonicNumber harmonicNumber = new HarmonicNumber();
-                    harmonicNumber.PrintHarmonicNum();
-                    break;
-                default:
-                    Console.WriteLine("Choose valid option");
-                    break;
+                Console.WriteLine("Choose 1.FlipCoin 2.LeapYear 3.PowerOfTwo 4.HarmonicNumber");
+                n = Convert.ToInt32(Console.ReadLine());
+                switch (n)
+                {
+                    case 1:
+                        FlipCoin flipCoin = new FlipCoin();
+                        flipCoin.CoinToss();
+                        break;
+                    case 2:
+                        CheckLeapYear checkLeapYear = new CheckLeapYear();
+                        checkLeapYear.CheckLeapYr();
+                        break;
+                    case 3:
+                        PowerOfTwo powerOfTwo = new PowerOfTwo();
+                        powerOfTwo.PowerOfTwoTable();
+                        break;
+                    case 4:
+                        HarmonicNumber harmonicNumber = new HarmonicNumber();
+                        harmonicNumber.PrintHarmonicNum();
+                        break;
+                    default:
+                        IsExitProgram = true;
+                        Console.WriteLine("Choose valid option");
+                        break;
+                }
             }
         }
     }
