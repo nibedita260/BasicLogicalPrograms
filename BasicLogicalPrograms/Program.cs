c1e9fed29e199c1ad0d1902d4cebc256be7b2713
nibedita260 <deepi.nibedita260.com>
2021-09-22 12:04:27 +0530

    [Nibedita]Added:FlipCoin Prog Feature Implemented

--- BasicLogicalPrograms/Program.cs
+++ BasicLogicalPrograms/Program.cs
@@ -6,18 +6,23 @@ namespace BasicLogicalPrograms
     {
         static void Main(string[] args)
         {
-            int N;
-            Console.WriteLine("Choose 1.FlipCoin");
-            N = Convert.ToInt32(Console.ReadLine());
-            switch (N)
+            int n;
+            bool IsExitProgram = false;
+            while (!IsExitProgram)
             {
-                case 1:
-                    FlipCoin flipCoin = new FlipCoin();
-                    flipCoin.CoinToss();
-                    break;
-                default:
-                    Console.WriteLine("Choose valid option");
-                    break;
+                Console.WriteLine("Choose 1.FlipCoin");
+                n = Convert.ToInt32(Console.ReadLine());
+                switch (n)
+                {
+                    case 1:
+                        FlipCoin flipCoin = new FlipCoin();
+                        flipCoin.CoinToss();
+                        break;
+                    default:
+                        IsExitProgram = true;
+                        Console.WriteLine("Choose valid option");
+                        break;
+                }
             }
         }
     }
